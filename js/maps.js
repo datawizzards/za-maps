e1f8a7d692be448995bbc932b5366e0e33ffaf9d
Ofentswe Lebogo <ofentswel@gmail.com>
2020-01-10 18:13:54 +0200

    Dynamically working from P->D->L, W needs fix

--- js/maps.js
+++ js/maps.js
@@ -1,103 +1,123 @@
 $(document).ready(function(){
 
+	var show = $('.button.active').val();
 
-    var svg = d3.select("svg"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+	 $('.button').click(function() {
+        $('.button').removeClass('active')
+        $(this).addClass('active')
 
-    var projection = d3.geoAlbers()
-        .center([24.7, -29.2])
-        .rotate([0, 0, 12])
-        .parallels([-22.1, -34.1])
-        .scale(3000)
-        .translate([200, 600]);
+        console.log($(this).val());
+        show = $(this).val();
+        visualize(show);
 
-    var path = d3.geoPath()
-        .projection(projection);
+    })
 
-    var color = d3.scaleThreshold()
-        .domain(d3.range(2, 10))
-        .range(d3.schemeBlues[9]);
+	visualize(show);
 
-    var colors = {
-        "Northern Cape": "#64B5F6", 
-        "Mpumalanga":"#4DD0E1", 
-        "KwaZulu-Natal":"#81C784",
-        "Western Cape": "#BA68C8",
-        "Free State": "#DCE775",
-        "North West": "#FFF176",
-        "Gauteng": "#F06292",
-        "Limpopo": "#E57373",
-        "Eastern Cape": "#FFB74D"
-    }
+	function visualize(show){
 
-    var data_points = {
-        "Northern Cape": 253, 
-        "Mpumalanga": 550, 
-        "KwaZulu-Natal": 800,
-        "Western Cape": 600,
-        "Free State": 563,
-        "North West": 234,
-        "Gauteng":  223,
-        "Limpopo": 123,
-        "Eastern Cape": 12
-    }
+		d3.select("svg").remove(); 
+		//var svg = d3.select("main").append("svg").attr("width","960").attr("height", "700"),
+	    var svg = d3.select("main").append("svg").attr("width","960").attr("height", "700"),
+	        width = +svg.attr("width"),
+	        height = +svg.attr("height");
 
-    var tooltip = d3.select('body')
-        .append('div')
-        .attr('class', 'tooltip')
-        .style('opacity', 0);
+	    var projection = d3.geoAlbers()
+	        .center([24.7, -29.2])
+	        .rotate([0, 0, 12])
+	        .parallels([-22.1, -34.1])
+	        .scale(3000)
+	        .translate([200, 600]);
 
-    var promises = {"Province": d3.json("geojson/za-provinces.topojson"),
-    	"District": d3.json("geojson/za-districts.topojson"),
-    	"Local": d3.json("geojson/za-local.topojson"),
-    	"Wards": d3.json("geojson/za-wards.topojson"),
-    }
+	    var path = d3.geoPath()
+	        .projection(projection);
 
-    Promise.all([promises["Wards"]]).then(ready)
+	    var color = d3.scaleThreshold()
+	        .domain(d3.range(2, 10))
+	        .range(d3.schemeBlues[9]);
 
-    // STRARTS HERE
-    function ready([data]){
+	    var colors = {
+	        "Northern Cape": "#64B5F6", 
+	        "Mpumalanga":"#4DD0E1", 
+	        "KwaZulu-Natal":"#81C784",
+	        "Western Cape": "#BA68C8",
+	        "Free State": "#DCE775",
+	        "North West": "#FFF176",
+	        "Gauteng": "#F06292",
+	        "Limpopo": "#E57373",
+	        "Eastern Cape": "#FFB74D"
+	    }
 
-        console.log(data);
-        console.log(topojson.feature(data, data.objects.layer1).features);
+	    var data_points = {
+	        "Northern Cape": 253, 
+	        "Mpumalanga": 550, 
+	        "KwaZulu-Natal": 800,
+	        "Western Cape": 600,
+	        "Free State": 563,
+	        "North West": 234,
+	        "Gauteng":  223,
+	        "Limpopo": 123,
+	        "Eastern Cape": 12
+	    }
 
-        svg.append("g")
-            .attr("class", "layer1")
-          .selectAll("path")
-          .data(topojson.feature(data, data.objects.layer1).features)
+	    var tooltip = d3.select('body')
+	        .append('div')
+	        .attr('class', 'tooltip')
+	        .style('opacity', 0);
 
-          .enter().append("path")
-            .attr("fill", d => '#19865C')//'#19865C') colors[d.properties.PROVINCE] ||
-            .attr("d", path)
-            .on('mouseover', function(d){
-                //console.log(d);
-                tooltip.transition()
-                  .duration(200)
-                  .style('opacity', 0.9);
+	    var promises = {"Province": d3.json("geojson/za-provinces.topojson"),
+	    	"District": d3.json("geojson/za-districts.topojson"),
+	    	"Local": d3.json("geojson/za-local.topojson"),
+	    	"Wards": d3.json("geojson/za-wards.topojson"),
+	    }
 
-                tooltip.html('<b>' + d.properties.PROVINCE + '</b><hr/>' + 'Number of people : ' + data_points[d.properties.PROVINCE])
-                  .style('left', d3.event.pageX + 'px')
-                  .style('top', d3.event.pageY - 28 +'px');
-              })
-            .on('mouseout', () => {
-              tooltip.transition()
-                .duration(500)
-                .style('opacity', 0);
-            });
-          // THIS IS TITLE TOOLTIP
-          // .append("title")
-          //   .text(d => d.properties.PROVINCE + ': ' + 7 + "%");
+	    Promise.all([promises[show]]).then(ready)
+
+	    d3.selectAll("g > *").remove()
+
+	    // STRARTS HERE
+	    function ready([data]){
+
+	        console.log(data);
+	        console.log(topojson.feature(data, data.objects.layer1).features);
+
+	        svg.append("g")
+	            .attr("class", "layer1")
+	          .selectAll("path")
+	          .data(topojson.feature(data, data.objects.layer1).features)
+
+	          .enter().append("path")
+	            .attr("fill", d => colors[d.properties.PROVINCE] || '#19865C')//'#19865C') colors[d.properties.PROVINCE] ||
+	            .attr("d", path)
+	            .on('mouseover', function(d){
+	                //console.log(d);
+	                tooltip.transition()
+	                  .duration(200)
+	                  .style('opacity', 0.9);
+
+	                tooltip.html('<b>' + d.properties.PROVINCE + '</b><hr/>' + 'Number of people : ' + data_points[d.properties.PROVINCE])
+	                  .style('left', d3.event.pageX + 'px')
+	                  .style('top', d3.event.pageY - 28 +'px');
+	              })
+	            .on('mouseout', () => {
+	              tooltip.transition()
+	                .duration(500)
+	                .style('opacity', 0);
+	            });
+	          // THIS IS TITLE TOOLTIP
+	          // .append("title")
+	          //   .text(d => d.properties.PROVINCE + ': ' + 7 + "%");
 
 
-        svg.append("path")
-            .datum(topojson.mesh(data, data.objects.layer1, (a, b) => a !== b))
-            .attr("class", "province")
-            //.attr("stroke-width", '10px')
-            .attr("d", path);
+	        svg.append("path")
+	            .datum(topojson.mesh(data, data.arcs, (a, b) => a !== b))
+	            .attr("class", "province")
+	            //.attr("stroke-width", '10px')
+	            .attr("d", path);
 
-    }
-    //ENDS HERE 
+	    }
+	    //ENDS HERE 
+	}
         
 
 
